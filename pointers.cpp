42458b25bfeb663ed8300969ed316d1fe4759c8e
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-02-24 18:10:37 +0530

    pointer basics

--- pointers.cpp
+++ pointers.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num = 5;
+    cout << "num is: " << num << endl;
+    cout << "address is: " << &num;
+    cout << endl;
 
+    cout << "Dealing with pointers\n";
+    // pointers
+    int *ptr = &num;
+    // pointer storing address of num
+    // it is int because num is int
+    // jiska adress store hota hai , usi type ka pointer banta hai
+
+    cout << ptr << endl; // address of num
+    cout << *ptr;        // it means value of num
+    // * is dereference operator
+    cout << endl;
+
+    /* ptr is a pointer to an integer, and it's assigned the address of num.
+ When you print *ptr, it dereferences the pointer, meaning it accesses the value stored
+ at the memory address ptr is pointing to, which in this case is the address of num.
+ Therefore, printing *ptr will give the value stored in num, which is 5.
+ So, printing *ptr will indeed give 5 */
+
+    // BETTER WAY FOR UNDERSTANDING AND DECLARING POINTER
+    int b = 10;
+    int *ptr1;
+    ptr1 = &b;
+
+    cout << "value of b is: " << *ptr1 << endl; // dereferencing the pointer
+    cout << "address is: " << ptr1 << endl;
+    return 0;
+}
