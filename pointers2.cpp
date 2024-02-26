6faa1ee4fc7eb5150153c0e67502a6584c7e2b9a
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-02-26 20:32:09 +0530

    pointers concept(in short)

---
+++ pointers2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num = 5;
+    int *ptr = &num;
+
+    (*ptr)++; // same as num++
+    cout << num << endl;
+}
+// ptr points to address of num
+// ptr = address of num
+// *ptr gives value at the address block where ptr was pointing
