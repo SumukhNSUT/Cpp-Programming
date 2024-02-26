fde87a09bc735d3ffebeb6be95a8058022f806f3
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-02-26 21:11:09 +0530

    practice of pointers

--- pointers_practice.cpp
+++ pointers_practice.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 int main()
 {
-    int arr[4] = {1, 2, 3, 4};
-    cout << "Value at 0th index is: " << *(arr) << endl;
-    cout << "Value at 0th index is: " << arr[0] << endl;
-    cout << "Address of 0th index is: " << &arr[0] << endl;
+     int arr[4] = {1, 2, 3, 4};
+     cout << "Value at 0th index is: " << *(arr) << endl;
+     cout << "Value at 0th index is: " << arr[0] << endl;
+     cout << "Address of 0th index is: " << &arr[0] << endl;
 
-    cout << endl
-         << endl;
+     cout << endl
+          << endl;
 
-    cout << "Value at 1st index is: " << *(arr + 1) << endl;
-    cout << "Value at 1st index is: " << arr[1] << endl;
-    cout << "Address of 1st index is: " << &arr[1] << endl;
+     cout << "Value at 1st index is: " << *(arr + 1) << endl;
+     cout << "Value at 1st index is: " << arr[1] << endl;
+     cout << "Address of 1st index is: " << &arr[1] << endl;
 
-    cout << endl
-         << endl;
+     cout << endl
+          << endl;
 
-    cout << "Value at 2nd index is: " << *(arr + 2) << endl;
-    cout << "Value at 2nd index is: " << arr[2] << endl;
-    cout << "Address of 2nd index is: " << &arr[2] << endl;
-    return 0;
+     cout << "Value at 2nd index is: " << *(arr + 2) << endl;
+     cout << "Value at 2nd index is: " << arr[2] << endl;
+     cout << "Address of 2nd index is: " << &arr[2] << endl;
+     return 0;
 }
