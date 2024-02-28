5595a8a462164a8ec623d4d49630a5bd0c1b4f91
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-02-28 17:06:35 +0530

    double pointers basic concept ( must read )

--- double_pointers.cpp
+++ double_pointers.cpp
@@ -7,4 +7,7 @@ int main()
     int *ptr = &i;
 
     int **ptr2 = &ptr; // double pointer
+    // a pointer ptr2 is created which stores address of ptr
+    // double star were used because address of a pointer ( ptr ) was stored
+    // ptr2 is known as double pointer declared by using **
 }
