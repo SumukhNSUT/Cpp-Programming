e38ae5b0a565f573c37fc61fd4c748feb176fc0d
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-03-06 14:13:14 +0530

    remove duplicates from a sorted vector/array

---
+++ remove_duplicates.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout << "Enter size: ";
+    cin >> size;
+
+    vector<int> v(size);
+    cout << "Enter elements:\n";
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> v[i];
+    }
+
+    // sort the vector
+    sort(v.begin(), v.end());
+
+    // check for duplicates
+    int unique_index = 0;
+    for (int i = 1; i < v.size(); i++)
+    {
+        if (v[i] != v[unique_index])
+        {
+            unique_index++;
+            v[unique_index] = v[i];
+        }
+    }
+    cout << "Without duplicates:\n";
+    for (int i = 0; i <= unique_index; i++) // loop until unique_index
+    {
+        cout << v[i] << " ";
+    }
+    return 0;
+}
