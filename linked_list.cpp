a70fdcef6f9009e41bf5cfd4755d8fdf293d75a2
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-03-06 13:31:56 +0530

    linked list

--- linked_list.cpp
+++ linked_list.cpp
@@ -3,15 +3,7 @@ using namespace std;
 
 int main()
 {
-    int *ptr1;
-    cout << "Size of pointer1: " << sizeof(ptr1) << " bytes" << endl;
-    char *ptr2;
-    cout << "Size of pointer2: " << sizeof(ptr2) << " bytes" << endl;
-    float *ptr3;
-    cout << "Size of pointer3: " << sizeof(ptr3) << " bytes" << endl;
-    double *ptr4;
-    cout << "Size of pointer4: " << sizeof(ptr4) << " bytes" << endl;
-    bool *ptr5;
-    cout << "Size of pointer5: " << sizeof(ptr5) << " bytes" << endl;
+    // linked list
+    
     return 0;
 }
