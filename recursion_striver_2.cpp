dd501c473ede5510b65e8a4048a14c795d8851d4
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-03-19 08:09:12 +0530

    check if a string is palindrome using recursion

--- recursion_striver_2.cpp
+++ recursion_striver_2.cpp
@@ -1,38 +1,77 @@
-#include <iostream>
-using namespace std;
-// reverse array
+// #include <iostream>
+// using namespace std;
+// // reverse array
 
-void reverse(int start, int end, int arr[], int size)
+// void reverse(int start, int end, int arr[], int size)
+// {
+//     if (start > end)
+//     { // base case
+//         return;
+//     }
+//     else
+//     {
+//         swap(arr[start], arr[end]);
+//         reverse(start + 1, end - 1, arr, size);
+//     }
+// }
+
+// int main()
+// {
+//     cout << "Enter size of array: ";
+//     int size;
+//     cin >> size;
+
+//     int arr[size];
+//     cout << "Enter elements:\n";
+//     for (int i = 0; i < size; i++)
+//     {
+//         cin >> arr[i];
+//     }
+//     reverse(0, size - 1, arr, size);
+//     // print reversed array
+//     cout << "Reversed array is -> ";
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+
+//     return 0;
+// }
+
+// check if a string is a palindrome
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool checkPalindrome(int start, int end, string s)
 {
-    if (start > end)
-    { // base case
-        return;
+    if (start >= end)
+    {
+        return true;
     }
     else
     {
-        swap(arr[start], arr[end]);
-        reverse(start + 1, end - 1, arr, size);
+        if (s[start] != s[end])
+        {
+            return false;
+        }
+        return checkPalindrome(start + 1, end - 1, s);
     }
 }
 
 int main()
 {
-    cout << "Enter size of array: ";
-    int size;
-    cin >> size;
+    string s1;
+    cout << "Enter a string: ";
+    cin >> s1;
 
-    int arr[size];
-    cout << "Enter elements:\n";
-    for (int i = 0; i < size; i++)
+    if (checkPalindrome(0, s1.length() - 1, s1))
     {
-        cin >> arr[i];
+        cout << s1 << " is a palindrome." << endl;
     }
-    reverse(0, size - 1, arr, size);
-    // print reversed array
-    cout << "Reversed array is -> ";
-    for (int i = 0; i < size; i++)
+    else
     {
-        cout << arr[i] << " ";
+        cout << s1 << " is not a palindrome." << endl;
     }
 
     return 0;
