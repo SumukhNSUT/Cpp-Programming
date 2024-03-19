ec685b6c6b22def8928caecd820debb8f086c5a5
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-03-19 07:51:43 +0530

    basic recursion concepts

--- recursion_striver.cpp
+++ recursion_striver.cpp
@@ -1,7 +1,56 @@
+// Print 1 to n using recursion
 #include <iostream>
 using namespace std;
+
+void print(int i, int n)
+{
+
+    if (i > n)
+    {
+        return;
+    }
+    else
+    {
+        cout << i << endl;
+        print(i + 1, n);
+    }
+}
+
+// reverse order
+void printCounting(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    else
+    {
+        cout << n;
+        printCounting(n - 1);
+    }
+}
+// sum of first n numbers
+int sum(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n + sum(n - 1);
+    }
+}
+
 int main()
 {
+    print(1, 5);
+
+    printCounting(5);
+    cout << endl;
+
+    int x = sum(5);
+    cout << x << endl;
 
     return 0;
 }
