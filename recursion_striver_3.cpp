2e92554c2d22c167e10c131f52d19d4084f896ed
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-03-21 10:57:55 +0530

    fib series

--- recursion_striver_3.cpp
+++ recursion_striver_3.cpp
@@ -1,33 +1,33 @@
-// print fibonacci series
-#include <iostream>
-using namespace std;
+// // print fibonacci series
+// #include <iostream>
+// using namespace std;
 
-int fib(int n)
-{
-    if (n <= 1)
-    {
-        return n;
-    }
-    else
-    {
-        return fib(n - 1) + fib(n - 2);
-    }
-}
+// int fib(int n)
+// {
+//     if (n <= 1)
+//     {
+//         return n;
+//     }
+//     else
+//     {
+//         return fib(n - 1) + fib(n - 2);
+//     }
+// }
 
-void print_fib(int n)
-{
-    for (int i = 0; i <= n; i++)
-    {
-        cout << fib(i) << " ";
-    }
-}
+// void print_fib(int n)
+// {
+//     for (int i = 0; i <= n; i++)
+//     {
+//         cout << fib(i) << " ";
+//     }
+// }
 
-int main()
-{
-    cout << "Enter n -> ";
-    int n;
-    cin >> n;
+// int main()
+// {
+//     cout << "Enter n -> ";
+//     int n;
+//     cin >> n;
 
-    print_fib(n);
-    return 0;
-}
+//     print_fib(n);
+//     return 0;
+// }
