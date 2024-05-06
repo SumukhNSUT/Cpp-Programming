4b1f6e361a0ba4956810063a9af10b3d9028d863
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-05-06 22:26:58 +0530

    basic node of a linked list

--- Linkedlist_1.cpp
+++ Linkedlist_1.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 using namespace std;
 
+class node
+{
+public:
+    int data;
+    node *next;
 
+    node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
 
 int main()
 {
+    node *node1 = new node(10);
+
+    cout << "Data: " << node1->data << endl;
+
+    delete node1;
 
     return 0;
 }
