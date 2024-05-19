5d49c2a4d7e80b32c43bbdf8b155bcd426195165
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-05-19 13:33:24 +0530

    circular linked list

---
+++ circular_linkedlist.cpp
@@ -0,0 +1,9 @@
+// circular linked list
+// same as singly linked list, just the last node points to 1st node instead of NULL
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    return 0;
+}
