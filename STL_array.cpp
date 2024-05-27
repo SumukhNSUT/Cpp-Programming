428f357f92563d5eae621b4b852ff6d1c48c6b85
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-05-28 00:03:36 +0530

    stl_arrays

--- STL_array.cpp
+++ STL_array.cpp
@@ -2,9 +2,8 @@
 #include <array>
 using namespace std;
 
-int main()
-{
-    array<int, 4> a = {1, 2, 3, 4};
+int main() {
+    array<int, 4> a = { 1, 2, 3, 4 };
     int size = a.size();
     cout << size << endl;
     cout << "Element at 2nd index is --> " << a.at(2) << endl;
