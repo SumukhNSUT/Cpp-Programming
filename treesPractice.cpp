7e65c5892ab479769a0ba9f0d6842eb72346c225
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-06-24 12:46:15 +0530

    building tree

--- treesPractice.cpp
+++ treesPractice.cpp
@@ -1,7 +1,38 @@
 #include<iostream>
 using namespace std;
 
+class node {
+public:
+    int data;
+    node* right;
+    node* left;
+
+    node(int d) {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+// function to create a tree
+node* buildTree(node* root) {
+    cout << "Enter data: " << endl;
+    int data;
+    cin >> data;
+    root = new node(data);
+
+    if (data == -1) {
+        return NULL;
+    }
+    cout << "Enter data for inserting in left: " << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter data for inserting in right :" << endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+
 int main() {
-    cout << "Hello";
+    node* root = NULL;
+    root = buildTree(root);
     return 0;
 }
