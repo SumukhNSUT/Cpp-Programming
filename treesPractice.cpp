4c92c35b7bd2baf4225051dc70bfa3c82b2ad432
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-06-24 17:01:06 +0530

    basic code application of tree

--- treesPractice.cpp
+++ treesPractice.cpp
@@ -1,38 +1,106 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-class node {
+// Definition for a binary tree node.
+class TreeNode {
 public:
-    int data;
-    node* right;
-    node* left;
+    int val;
+    TreeNode* left;
+    TreeNode* right;
 
-    node(int d) {
-        this->data = d;
-        this->left = NULL;
-        this->right = NULL;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+// Class definition for binary tree
+class BinaryTree {
+public:
+    TreeNode* root;
+
+    BinaryTree() : root(nullptr) {}
+
+    // Function to insert a node in the binary tree
+    TreeNode* insert(TreeNode* node, int val) {
+        if (node == nullptr) {
+            return new TreeNode(val);
+        }
+        if (val < node->val) {
+            node->left = insert(node->left, val);
+        }
+        else {
+            node->right = insert(node->right, val);
+        }
+        return node;
+    }
+
+    void insert(int val) {
+        root = insert(root, val);
+    }
+
+    // Function to perform in-order traversal of the binary tree
+    void inOrder(TreeNode* node) const {
+        if (node != nullptr) {
+            inOrder(node->left);
+            cout << node->val << " ";
+            inOrder(node->right);
+        }
+    }
+
+    void inOrder() const {
+        inOrder(root);
+        cout << endl;
+    }
+
+    // Function to perform pre-order traversal of the binary tree
+    void preOrder(TreeNode* node) const {
+        if (node != nullptr) {
+            cout << node->val << " ";
+            preOrder(node->left);
+            preOrder(node->right);
+        }
+    }
+
+    void preOrder() const {
+        preOrder(root);
+        cout << endl;
+    }
+
+    // Function to perform post-order traversal of the binary tree
+    void postOrder(TreeNode* node) const {
+        if (node != nullptr) {
+            postOrder(node->left);
+            postOrder(node->right);
+            cout << node->val << " ";
+        }
+    }
+
+    void postOrder() const {
+        postOrder(root);
+        cout << endl;
     }
 };
 
-// function to create a tree
-node* buildTree(node* root) {
-    cout << "Enter data: " << endl;
-    int data;
-    cin >> data;
-    root = new node(data);
-
-    if (data == -1) {
-        return NULL;
-    }
-    cout << "Enter data for inserting in left: " << endl;
-    root->left = buildTree(root->left);
-    cout << "Enter data for inserting in right :" << endl;
-    root->right = buildTree(root->right);
-    return root;
-}
-
 int main() {
-    node* root = NULL;
-    root = buildTree(root);
+    BinaryTree tree;
+
+    // Insert nodes into the tree
+    tree.insert(50);
+    tree.insert(30);
+    tree.insert(20);
+    tree.insert(40);
+    tree.insert(70);
+    tree.insert(60);
+    tree.insert(80);
+
+    // Perform traversals
+    cout << "In-order traversal: ";
+    tree.inOrder();
+
+    cout << "Pre-order traversal: ";
+    tree.preOrder();
+
+    cout << "Post-order traversal: ";
+    tree.postOrder();
+
     return 0;
 }
