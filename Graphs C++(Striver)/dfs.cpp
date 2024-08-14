53de9ebf0098ae3c6658447739ea22ac358b9c81
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-08-14 13:55:32 +0530

    dfs traversal of graph

--- Graphs C++(Striver)/dfs.cpp
+++ Graphs C++(Striver)/dfs.cpp
@@ -1,7 +1,50 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
+void dfsUtil(int node, vector<int> adj[], vector<int>& vis, vector<int>& result) {
+    vis[node] = 1;
+    result.push_back(node);
+
+    // Traverse neighbors
+    for (int it : adj[node]) {
+        if (!vis[it]) {
+            dfsUtil(it, adj, vis, result);
+        }
+    }
+}
+
+vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+    vector<int> vis(V, 0);
+    int start = 0;
+    vector<int> result;
+    dfsUtil(start, adj, vis, result);
+    return result;
+}
+
 int main() {
+    int V = 5;
+    vector<int> adj[V];
+
+    // Example graph
+    adj[0].push_back(1);
+    adj[1].push_back(0);
+
+    adj[0].push_back(2);
+    adj[2].push_back(0);
+
+    adj[1].push_back(3);
+    adj[3].push_back(1);
+
+    adj[2].push_back(4);
+    adj[4].push_back(2);
+
+    vector<int> result = dfsOfGraph(V, adj);
+
+    // Print DFS traversal
+    for (int i : result) {
+        cout << i << " ";
+    }
 
     return 0;
 }
