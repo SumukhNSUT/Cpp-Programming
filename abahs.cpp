ad9455e02c1de64e2911cee9cd3f1d70f93a5747
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2025-07-12 01:18:45 +0530

    COMMIT

--- abahs.cpp
+++ abahs.cpp
@@ -1,69 +1,64 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <string>
 #include <algorithm>
+
 using namespace std;
 
-vector<vector<int>> grid;
-vector<vector<int>> paths;
-int n, target;
-
-void findPaths(int row, int col, int currentSum, vector<int>& currentPath) {
-    // Add current cell value to sum and path
-    currentSum += grid[row][col];
-    currentPath.push_back(grid[row][col]);
-
-    // If we reached bottom-right corner
-    if (row == n - 1 && col == n - 1) {
-        if (currentSum == target) {
-            paths.push_back(currentPath);
-        }
-        currentPath.pop_back();
-        return;
-    }
-
-    // Move right if possible
-    if (col + 1 < n) {
-        findPaths(row, col + 1, currentSum, currentPath);
-    }
-
-    // Move down if possible
-    if (row + 1 < n) {
-        findPaths(row + 1, col, currentSum, currentPath);
-    }
-
-    // Backtrack
-    currentPath.pop_back();
-}
+struct Task {
+    char id;
+    int effort;
+    int reward;
+};
 
 int main() {
-    cin >> n;
-    grid.resize(n, vector<int>(n));
+    int N, Smax;
+    cin >> N >> Smax;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> grid[i][j];
-        }
+    vector<Task> tasks(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> tasks[i].id >> tasks[i].effort >> tasks[i].reward;
     }
 
-    cin >> target;
+    // DP[i][j] = max reward using first i tasks with j stamina
+    vector<vector<int>> dp(N + 1, vector<int>(Smax + 1, 0));
+    vector<vector<bool>> take(N + 1, vector<bool>(Smax + 1, false));
 
-    vector<int> currentPath;
-    findPaths(0, 0, 0, currentPath);
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 0; j <= Smax; ++j) {
+            // Not taking task i
+            dp[i][j] = dp[i - 1][j];
 
-    if (paths.empty()) {
-        cout << "No path found" << endl;
-    }
-    else {
-        for (auto& path : paths) {
-            cout << "Path Found: ";
-            for (int i = 0; i < path.size(); i++) {
-                cout << path[i];
-                if (i < path.size() - 1) cout << " ";
+            if (j >= tasks[i - 1].effort) {
+                int val = dp[i - 1][j - tasks[i - 1].effort] + tasks[i - 1].reward;
+                if (val > dp[i][j]) {
+                    dp[i][j] = val;
+                    take[i][j] = true;
+                }
             }
-            cout << endl;
         }
+    }
+
+    // Backtrack to get selected tasks
+    vector<char> selected;
+    int totalEffort = 0, totalReward = 0;
+    int i = N, j = Smax;
+    while (i > 0) {
+        if (take[i][j]) {
+            selected.push_back(tasks[i - 1].id);
+            totalEffort += tasks[i - 1].effort;
+            totalReward += tasks[i - 1].reward;
+            j -= tasks[i - 1].effort;
+        }
+        --i;
+    }
+
+    if (selected.empty()) {
+        cout << -1 << endl;
+    } else {
+        reverse(selected.begin(), selected.end());
+        for (char c : selected) cout << c << " ";
+        cout << "\n" << totalEffort << " " << totalReward << endl;
     }
 
     return 0;
